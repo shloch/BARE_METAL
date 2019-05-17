4e499a6fa97a3b27470fa8a9020d8d8fb0aab87a
SHEY LOUIS CHIA <shloch2007@yahoo.fr>
2019-05-17 15:13:25 +0100

    Editing user data

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,6 +14,17 @@ class UsersController < ApplicationController
         end
       end
 
+      def edit
+        @user = User.find(params[:id])
+      end
+
+      def update
+        #raise params.inspect
+        @user = User.find(params[:id])
+        @user.update(user_params)
+        redirect_to "/users/#{params[:id]}/edit"
+      end
+
       private
 
       def user_params
